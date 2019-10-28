cac4381534736a2c798c2b293a8debb84de5652d
Rubén de Celis Hernández <contact@rdch106.hol.es>
2019-10-28 02:08:32 +0100

    Add --server argument

--- postmancli/core.py
+++ postmancli/core.py
@@ -2,6 +2,7 @@
 
 import argparse
 from postmancli.sendemail import send_mail
+from postmancli.validation import Validator
 from postmancli.metadata import Metadata
 
 
@@ -13,6 +14,9 @@ class Postman(object):
         # Parse arguments provided
         parser = argparse.ArgumentParser()
         parser.add_argument('-v', '--version', action='version', version=self.meta.get_version())
+        parser.add_argument('-s', '--server', dest='server',
+                            help="SMTP server with format user:password@smtpserver:port",
+                            type=Validator.check_server, required=True)
         self.args = parser.parse_args()
 
     def run(self):
