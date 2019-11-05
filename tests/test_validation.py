eea4b2e5f364e50a35c1500971bd364064323647
rdecelis <rdecelis@stt-systems.com>
2019-11-05 14:44:17 +0100

    Add ✅ test_server

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -22,6 +22,48 @@ class TestClassValidations(unittest.TestCase):
         except ArgumentTypeError as e:
             self.assertIsInstance(e, ArgumentTypeError)
 
+    def test_server(self):
+        self.assertEqual('user:password@domain.com:443',
+                         Validator.check_server('user:password@domain.com:443'))
+        self.assertEqual('user:password@subdomain.domain.com:443',
+                         Validator.check_server('user:password@subdomain.domain.com:443'))
+        self.assertEqual('user:password@more-domain.com:443',
+                         Validator.check_server('user:password@more-domain.com:443'))
+        self.assertEqual('user:password@subdomain.more-domain.com:443',
+                         Validator.check_server('user:password@subdomain.more-domain.com:443'))
+        self.assertEqual('user.1:password.1@domain.com:443',
+                         Validator.check_server('user.1:password.1@domain.com:443'))
+        self.assertEqual('user.1:password.1@more-domain.com:443',
+                         Validator.check_server('user.1:password.1@more-domain.com:443'))
+        self.assertEqual('user.1:password.1@subdomain.domain.com:443',
+                         Validator.check_server('user.1:password.1@subdomain.domain.com:443'))
+        self.assertEqual('user.1:password.1@more-subdomain.domain.com:443',
+                         Validator.check_server('user.1:password.1@more-subdomain.domain.com:443'))
+        self.assertEqual('user.1:password.1@subdomain.more-domain.com:443',
+                         Validator.check_server('user.1:password.1@subdomain.more-domain.com:443'))
+        self.assertEqual('user.1:password.1@more-subdomain.more-domain.com:443',
+                         Validator.check_server('user.1:password.1@more-subdomain.more-domain.com:443'))
+        try:
+            Validator.check_server('domain.com:443')
+            self.fail('Exception not raised')
+        except ArgumentTypeError as e:
+            self.assertIsInstance(e, ArgumentTypeError)
+        try:
+            Validator.check_server('user:password@domain.com')
+            self.fail('Exception not raised')
+        except ArgumentTypeError as e:
+            self.assertIsInstance(e, ArgumentTypeError)
+        try:
+            Validator.check_server('user@domain.com:443')
+            self.fail('Exception not raised')
+        except ArgumentTypeError as e:
+            self.assertIsInstance(e, ArgumentTypeError)
+        try:
+            Validator.check_server('user:password@-domain.com:443')
+            self.fail('Exception not raised')
+        except ArgumentTypeError as e:
+            self.assertIsInstance(e, ArgumentTypeError)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
