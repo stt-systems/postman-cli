84336ca91bc38296488181951c27a9fabd663587
rdecelis <rdecelis@stt-systems.com>
2019-11-06 17:21:05 +0100

    Add module invocation behaviour

---
+++ postmancli/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Execute with:
+# $ python -m postmancli (2.7+)
+
+import postmancli.main
+
+if __name__ == "__main__":
+    postmancli.main.main()
---
+++ postmancli/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import postmancli.core
+
+
+def main():
+    postmancli.core.Postman().run()
